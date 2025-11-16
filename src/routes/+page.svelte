9aafbcf7296e5fc13d4fe0035191d80c6d79fdb6
Andrew Chang <21andrewch@gmail.com>
2025-11-16 09:14:09 -0600

    feat: login zindex

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -141,7 +141,7 @@
 		onclick={() => {
 			openAuthModal();
 		}}
-		class="fixed top-3 left-3 flex items-center gap-2 rounded-md px-1 py-1 text-stone-700 transition hover:bg-stone-200 hover:text-stone-900"
+		class="fixed top-3 left-3 z-[999] flex items-center gap-2 rounded-md px-1 py-1 text-stone-700 transition hover:bg-stone-200 hover:text-stone-900"
 		aria-label="Go to home"
 	>
 		<img src={vectorUrl} alt="vector" class="h-5 w-5 opacity-80" />
